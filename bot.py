d515068b99ef1d59e543de2b5c1b6b235494eec8
James Wu <jameswu144@gmail.com>
2024-12-15 17:50:29 +0000

    implement h17 strategy

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from player import Player
 import random
+from strategy import h17_strategy, s17_strategy
 
 
 class RandomBot(Player):
@@ -15,15 +16,43 @@ class RandomBot(Player):
 
 
 class CardCounter(Player):
-    def __init__(self, bankroll, observer):
+    def __init__(self, bankroll, observer, ruleset):
         super().__init__(bankroll)
         self.observer = observer
 
-    def move(self, hand, dealer_hand, legal_moves):
-        dealer_visible_card_rank = dealer_hand.cards[0].rank
+        if ruleset.threshold != 17:
+            raise NotImplementedError("Only 17 threshold implemented")
 
-        # Implement basic strategy
-        raise NotImplementedError
+        self.ruleset = ruleset
+        self.strategy = s17_strategy if ruleset.stand_soft else h17_strategy
+
+    def move(self, hand, dealer_hand, legal_moves):
+        dealer_value = dealer_hand.cards[0].get_value()
+
+        total = hand.total
+
+        if 'split' in legal_moves:
+            first_card_value = hand[0].get_value()
+            option = self.strategy['pairs'][first_card_value][dealer_value]
+            if option == 'Y': return 'split'
+            if option == 'M' and self.ruleset.das: return 'split'
+            if option == 'R':
+                return 'surrender' if 'surrender' in legal_moves else 'hit'
+
+        table = 'soft' if hand.soft_aces else 'hard'
+        option = self.strategy[table][total][dealer_value]
+        if option == 'H': return 'hit'
+        if option == 'S': return 'stand'
+        if option == 'D':
+            return 'double' if 'double' in legal_moves else 'hit'
+        if option == 'F':
+            return 'double' if 'double' in legal_moves else 'stand'
+        if option == 'R':
+            return 'surrender' if 'surrender' in legal_moves else 'hit'
+        if option == 'V':
+            return 'surrender' if 'surrender' in legal_moves else 'stand'
+
+        raise ValueError(f'Invalid option: {option}')
 
     def get_bet(self):
         true_count = self.observer.get_true_count()
